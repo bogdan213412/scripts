ef8bcd35d50eaa4611668be52709a2113af2f1e7
bogdan213412 <111419151+bogdan213412@users.noreply.github.com>
2023-12-17 10:25:27 +0300

    Add files via upload

---
+++ script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    void Start()
+    {
+        //пишет hello world
+        print("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
